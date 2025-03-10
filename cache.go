792c8fc0d8dbaea575dd10664cdd57a70d4db535
subrajeet-maharana <subrajeet.info@gmail.com>
2025-03-10 21:55:50 +0530

    added benchmarking for hit/miss

--- cache.go
+++ cache.go
@@ -1,6 +1,12 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
 
 type Cache struct {
 	mu    sync.RWMutex
@@ -30,4 +36,42 @@ func (c *Cache) Delete(key string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	delete(c.store, key)
+}
+
+func BenchmarkCache(cache *Cache, numReaders, numWriters, iterations int) {
+	var wg sync.WaitGroup
+	start := time.Now()
+
+	for i := range numWriters {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for j := 0; j < iterations; j++ {
+				key := fmt.Sprintf("Key-%d", rand.Intn(10))
+				value := fmt.Sprintf("Value-%d", rand.Intn(100))
+				cache.Set(key, value)
+				log.Printf("[Writer-%d] SET %s -> %s\n", id, key, value)
+				time.Sleep(time.Microsecond * 10)
+			}
+		}(i)
+	}
+
+	for i := range numReaders {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for j := 0; j < iterations; j++ {
+				key := fmt.Sprintf("Key-%d", rand.Intn(10))
+				if value, found := cache.Get(key); found {
+					log.Printf("[Reader-%d] HIT %s -> %s\n", id, key, value)
+				} else {
+					log.Printf("[Reader-%d] MISS %s\n", id, key)
+				}
+				time.Sleep(time.Microsecond * 10)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Printf("Benchmark completed in %v\n", time.Since(start))
 }
